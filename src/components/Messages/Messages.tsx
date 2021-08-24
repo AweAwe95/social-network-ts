353350ced3aa0da1aba37542cdef79187f5b24c6
AweAwe95 <vitalikarach95@gmail.com>
2021-08-24 12:13:49 +0300

    add types for social-network -54, first response(axios) in Users

--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -1,22 +1,32 @@
 import m from "./Messages.module.css"
 import {Friend} from "./Friend/Friend";
 import {Message} from "./Message/Message";
-import {MessagesPageDataTypes} from "../../redux/state";
+import {ChangeEvent} from "react";
+import {messagesPageType} from "../../redux/messages-reducer";
 
-
-type MessagesPropsTypes = {
-    messagesPageData: MessagesPageDataTypes
+type MessagesType = {
+    messagesPageData: messagesPageType
+    addMessage: () => void
+    textAreaHandler: (newMessage: string) => void
 }
 
-export function Messages(props: MessagesPropsTypes) {
 
+export function Messages(props: MessagesType) {
+    const textAreaHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        props.textAreaHandler(e.currentTarget.value)
+    }
+    const addMessage = () => {
+        props.addMessage()
+    }
     return (
         <div className={m.content}>
             <div className={m.friends}>
-                {props.messagesPageData.friendsData.map(f => <Friend id={f.id} name={f.name}/>)}
+                {props.messagesPageData.friendsData.map(f => <Friend key={f.id} id={f.id} name={f.name}/>)}
             </div>
             <div>
-                {props.messagesPageData.messagesData.map(m => <Message message={m.message}/>)}
+                {props.messagesPageData.messagesData.map(m => <Message key={m.id} message={m.message}/>)}
+                <input value={props.messagesPageData.newMessageText} onChange={textAreaHandler}/>
+                <button onClick={addMessage}>Add Message</button>
             </div>
         </div>
     )
